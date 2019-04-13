22ca3ab795c905ef7fd28efcea4ea75e0edcd8b7
Karan Singh Saini <karansingh7613@gmail.com>
2019-04-13 22:28:48 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var todoController = require('./controller/todoController');
+var app = express();
+
+//set up template engine
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
+//fire controllers
+todoController(app);
+
+//listen to port
+app.listen(3000);
+console.log("Listening to port 3000");
